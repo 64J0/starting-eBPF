384b77c7729b259beb05057d7dd7a79713d2cd48
64J0 <vinigaio97@gmail.com>
2023-06-21 17:06:06 -0300

    chore: add hello.py program with a simple BCC application

---
+++ chapter-2/hello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Basically, the Python program compiles the C code, loads it into the kernel,
+# and attaches it to the execve syscall kprobe. Whenever any application on this
+# (virtual) machine calls execve() it triggers the eBPF hello() program, which
+# writes a line of trace into a specific pseudofile. The Python program reads
+# the trace message from the pseudofile and displays it to the user.
+
+# BCC installation instructions:
+# https://github.com/iovisor/bcc/blob/master/INSTALL.md
+
+from bcc import BPF
+
+program = r"""
+int hello(void *ctx) {
+    bpf_trace_printk("Hello World!");
+    return 0;
+}
+"""
+
+b = BPF(text=program)
+# execve is the syscall used to execute a program
+syscall = b.get_syscall_fnname("execve")
+b.attach_kprobe(event=syscall, fn_name="hello")
+
+b.trace_print()
+
+# hello.py -> user space part of the application
+# hello()  -> eBPF program that runs in the kernel
